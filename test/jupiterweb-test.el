fc8085c4ecb5ea298cbaa60a4ff924f03d01acb2
rodrigues-am <rodrigues.am@usp.br>
2026-06-21 21:55:32 -0300

    JW-010 JW-011 JW-012 DONE: package entry point, variables, URL builders + tests
    JW-010: (require 'jupiterweb) loads without errors
    JW-011: all 13 defcustom variables verified
    JW-012: URL builders and course key implemented + 3 ERT tests passing
    JW-013 marked NEXT.

--- test/jupiterweb-test.el
+++ test/jupiterweb-test.el
@@ -20,7 +20,33 @@
   :type 'boolean
   :group 'jupiterweb)
 
-;; Tests will be added as features are implemented.
+;;; URL builders
+
+(ert-deftest jupiterweb-test-grade-url ()
+  "Test that `jupiterweb--grade-url' builds the correct curriculum URL."
+  (should (equal (jupiterweb--grade-url)
+                 "https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=43&codcur=43031&codhab=0&tipo=N"))
+  ;; With explicit arguments
+  (should (equal (jupiterweb--grade-url "43" "43031" "0" "N")
+                 "https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=43&codcur=43031&codhab=0&tipo=N"))
+  ;; With different course
+  (should (equal (jupiterweb--grade-url "17" "17001" "0" "N")
+                 "https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=17&codcur=17001&codhab=0&tipo=N")))
+
+(ert-deftest jupiterweb-test-discipline-url ()
+  "Test that `jupiterweb--discipline-url' builds the correct discipline URL."
+  (should (equal (jupiterweb--discipline-url "4300151")
+                 "https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=4300151&codcur=43031&codhab=0"))
+  ;; With explicit arguments
+  (should (equal (jupiterweb--discipline-url "4300151" "43031" "0")
+                 "https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=4300151&codcur=43031&codhab=0"))
+  ;; Different discipline and course
+  (should (equal (jupiterweb--discipline-url "4300152" "43031" "4")
+                 "https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=4300152&codcur=43031&codhab=4")))
+
+(ert-deftest jupiterweb-test-current-course-key ()
+  "Test that `jupiterweb--current-course-key' returns the correct key."
+  (should (equal (jupiterweb--current-course-key) '("43031" . "0"))))
 
 (provide 'jupiterweb-test)
 ;;; jupiterweb-test.el ends here
